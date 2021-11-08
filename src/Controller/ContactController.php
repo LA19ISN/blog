b890a92a58667d07b15bda508b58f08f451141e8
LA19ISN <titoulelio@live.fr>
2021-11-08 15:05:46 +0100

    Exo 1 php symfony

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactCity(Request $request, string $city): Response
+    {
+        $name = $request->query->get('name');
+
+        return $this->render('contact/index.html.twig', [
+            'name' => $name,
+            'city' => $city,
+        ]);
+    }
+
+    /**
+     * @Route("/contact/{city}", name="contact")
+     */
+
+}
