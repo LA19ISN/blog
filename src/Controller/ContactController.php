298f6a2dbccde4ed8b53dc5ff0ac0bf8f515e16e
LA19ISN <titoulelio@live.fr>
2021-11-24 12:02:19 +0100

    Mise à jour jusqu'à l'exo 7 symfony

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,27 +2,46 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @var ContactRepository
+     */
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository)
+    {
+        $this->contactRepository = $contactRepository;
+    }
+
+    /**
+     * @Route("/contactez-nous", name="contact")
+     */
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'name' => 'Lelio',
+            'contacts' => $this->contactRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/contacter/{city}", name="contactCity")
      */
     public function contactCity(Request $request, string $city): Response
     {
         $name = $request->query->get('name');
+        dump($name);
 
         return $this->render('contact/index.html.twig', [
             'name' => $name,
             'city' => $city,
         ]);
     }
-
-    /**
-     * @Route("/contact/{city}", name="contact")
-     */
-
 }
